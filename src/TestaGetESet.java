aecd8a3a8f8c6460ddd6cb658c25aa04672870dc
luccasrolland <luccas.rolland@gmail.com>
2022-08-03 20:05:31 -0300

    "testing methods"

--- src/TestaGetESet.java
+++ src/TestaGetESet.java
@@ -3,5 +3,12 @@ public class TestaGetESet {
         Conta conta = new Conta();
         conta.setNumero(1337);
         System.out.println(conta.getNumero());
+
+        Cliente lucas = new Cliente();
+        lucas.setNome("Lucas Roland");
+
+        conta.setTitular(lucas);
+        System.out.println(conta.getTitular().getNome());
+        conta.getTitular().setProfissao("Progamador");
     }
 }
