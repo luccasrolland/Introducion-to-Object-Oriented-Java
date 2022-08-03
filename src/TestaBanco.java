90c1a4fb2251dcf8a5353b569e438ca0b1210a7a
luccasrolland <luccas.rolland@gmail.com>
2022-08-03 20:03:51 -0300

    "testing methods"

--- src/TestaBanco.java
+++ src/TestaBanco.java
@@ -1,15 +1,14 @@
 public class TestaBanco {
     public static void main(String[] args) {
         Cliente lucas = new Cliente();
-        lucas.nome = "Lucas Roland";
-        lucas.cpf = "000.000.000-00";
-        lucas.profissao = "Gerente Administrativo/Programador";
+        lucas.setNome("Lucas Roland");
+        lucas.setCpf("000.000.000-00");
+        lucas.setProfissao("Gerente Administrativo/Programador");
 
         Conta contaDoLucas = new Conta();
         contaDoLucas.deposita(2750);
 
-        contaDoLucas.titular = lucas;
-        System.out.println(contaDoLucas.titular);
+
 
     }
 }
