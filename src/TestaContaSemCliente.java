0ea7416a9cb3ffe8ee934915cb1ce1b5dcb44308
luccasrolland <luccas.rolland@gmail.com>
2022-08-03 22:35:03 -0300

    "testing constructors"

--- src/TestaContaSemCliente.java
+++ src/TestaContaSemCliente.java
@@ -1,6 +1,6 @@
 public class TestaContaSemCliente {
     public static void main(String[] args) {
-        Conta contaDaSteffany = new Conta();
+        Conta contaDaSteffany = new Conta(1338, 017);
         System.out.println(contaDaSteffany.getSaldo());
 
 
